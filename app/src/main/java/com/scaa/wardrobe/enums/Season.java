30ad71a66e67fcad2ad88a6e2d7044b06dc8d217
Scavia Charles Mhlanga <scaviacmhlanga@gmail.com>
2024-08-26 08:53:43 +0200

    add seasons enum and add season type to cloth

--- app/src/main/java/com/scaa/wardrobe/enums/Season.java
+++ app/src/main/java/com/scaa/wardrobe/enums/Season.java
@@ -1,6 +1,19 @@
 package com.scaa.wardrobe.enums;
 
 public enum Season {
-    SUMMER,
-    WINTER
+    SUMMER("summer", 0),
+    WINTER("winter", 1);
+
+    private String stringValue;
+    private int intValue;
+
+    private Season(String toString, int value) {
+        stringValue = toString;
+        intValue = value;
+    }
+
+    @Override
+    public String toString() {
+        return stringValue;
+    }
 }
