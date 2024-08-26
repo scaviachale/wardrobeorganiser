0ca9a2a92afc4b81a8437b5c55f6ae04663b8af6
Scavia Charles Mhlanga <scaviacmhlanga@gmail.com>
2024-08-26 17:53:25 +0200

    -add parent activity -destroy all activities on logout

--- app/src/main/java/com/scaa/wardrobe/WardrobeActivity.java
+++ app/src/main/java/com/scaa/wardrobe/WardrobeActivity.java
@@ -1,18 +1,10 @@
 package com.scaa.wardrobe;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class WardrobeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-    BottomNavigationView bottomNavigationView;
+public class WardrobeActivity extends WardrobeOrganiserApplicationActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,33 +13,5 @@ public class WardrobeActivity extends AppCompatActivity implements BottomNavigat
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.menuHome);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // action with ID action_refresh was selected
-            case R.id.menuHome:
-
-                break;// action with ID action_refresh was selected
-            case R.id.menuToday:
-
-                break;// action with ID action_refresh was selected
-            case R.id.menuServices:
-
-                break;
-            // action with ID action_settings was selected
-            case R.id.menuLogout:
-                Toast.makeText(WardrobeActivity.this, "LogOut Successful", Toast.LENGTH_SHORT)
-                        .show();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            default:
-                break;
-        }
-
-        return true;
     }
 }
