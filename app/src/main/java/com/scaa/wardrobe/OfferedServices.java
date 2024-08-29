5831b4853b7ed7c5375bc4da72ef283762007758
Scavia Charles Mhlanga <scaviacmhlanga@gmail.com>
2024-08-29 15:17:09 +0200

    fix navigation on home activity

--- app/src/main/java/com/scaa/wardrobe/OfferedServices.java
+++ app/src/main/java/com/scaa/wardrobe/OfferedServices.java
@@ -1,10 +1,12 @@
 package com.scaa.wardrobe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
 
 public class OfferedServices extends Fragment {
@@ -16,6 +18,13 @@ public class OfferedServices extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                startActivity(new Intent(getActivity(), HomeActivity.class));
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
         return inflater.inflate(R.layout.fragment_offered_services, container, false);
     }
 }
